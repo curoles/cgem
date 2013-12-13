433da020296f62c2d19dbcb0bb8e2a077fa7594b
curoles <curoles@yahoo.com>
2013-12-12 19:34:46 -0800

    logging

--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
-CXX := g++ #/tools/local/gcc-4.7.2/bin/g++-4.7.2
-CC := gcc # /tools/local/gcc-4.7.2/bin/gcc-4.7.2
-CXXFLAGS := -std=c++0x #-std=c++11 -L/tools/local/gcc-4.7.2/lib64
+CXX := /tools/local/gcc-4.7.2/bin/g++-4.7.2
+CC := /tools/local/gcc-4.7.2/bin/gcc-4.7.2
+CXXFLAGS := -std=c++11 -L/tools/local/gcc-4.7.2/lib64
